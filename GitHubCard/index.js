675ce6a29de2ebb55c27735e08ecd21b404a798e
A <jesus.e.estrada@hotmail.com>
2020-08-06 15:11:33 -0500

    made and apended elements in cardMaker function

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,8 +1,16 @@
+import axios from 'axios'
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+
+// axios.get('https://api.github.com/users/EdwardEJ')
+//   .then(response => {
+
+//   })
+//   .catch(err => {
+//   })
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -30,25 +38,60 @@
 
 const followersArray = [];
 
+
+
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
 
-    <div class="card">
-      <img src={image url of user} />
-      <div class="card-info">
-        <h3 class="name">{users name}</h3>
-        <p class="username">{users user name}</p>
-        <p>Location: {users location}</p>
-        <p>Profile:
+    <div class="card">                            //card
+      <img src={image url of user} />             //cardImg
+      <div class="card-info">                     //cardInfo
+        <h3 class="name">{users name}</h3>        //cardName
+        <p class="username">{users user name}</p> //cardUserName
+        <p>Location: {users location}</p>         //cardLocation
+        <p>Profile:                               //cardProfile
+                                                  //cardHTMLURL
           <a href={address to users github page}>{address to users github page}</a>
         </p>
-        <p>Followers: {users followers count}</p>
-        <p>Following: {users following count}</p>
-        <p>Bio: {users bio}</p>
+        <p>Followers: {users followers count}</p> //cardFollowers
+        <p>Following: {users following count}</p> //cardFollowing
+        <p>Bio: {users bio}</p>                   //cardBio
       </div>
     </div>
 */
+
+function cardMaker(cardObj) {
+
+  const card = document.createElement('div')
+  const cardImg = document.createElement('img')
+  const cardInfo = document.createElement('div')
+  const cardName = document.createElement('h3')
+  const cardUserName = document.createElement('p')
+  const cardLocation = document.createElement('p')
+  const cardProfile = document.createElement('p')
+  const cardHtmlURL = document.createElement('a')
+  const cardFollowers = document.createElement('p')
+  const cardFollowing = document.createElement('p')
+  const cardBio = document.createElement('p')
+
+  card.appendChild(cardImg)
+  card.appendChild(cardInfo)
+  cardInfo.appendChild(cardName)
+  cardInfo.appendChild(cardUserName)
+  cardInfo.appendChild(cardLocation)
+  cardInfo.appendChild(cardProfile)
+  cardProfile.appendChild(cardHtmlURL)
+  cardInfo.appendChild(cardFollowers)
+  cardInfo.appendChild(cardFollowing)
+  cardInfo.appendChild(cardBio)
+
+  card.classList.add(card)
+  cardImg.setAttribute('src', cardObj['avatar_url']);
+
+  return card
+}
+cardMaker()
 
 /*
   List of LS Instructors Github username's:
